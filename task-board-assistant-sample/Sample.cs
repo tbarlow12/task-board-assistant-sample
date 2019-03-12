24e9e28f079fc87cf0b76051dc542417f66eca1a
Tanner Barlow <tanner.barlow12@gmail.com>
2019-03-12 13:17:57 -0700

    Executing a policy locally

--- task-board-assistant-sample/Sample.cs
+++ task-board-assistant-sample/Sample.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskBoardAssistant;
 
 namespace TaskBoardAssistantSample
 {
@@ -10,6 +11,7 @@ namespace TaskBoardAssistantSample
     {
         static void Main(string[] args)
         {
+            Assistant.ExecuteFromYmlPath("../../policy.yml");
         }
     }
 }
